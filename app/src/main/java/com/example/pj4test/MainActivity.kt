8e413053f25739dd8c5ba36e8b6f336f425e853c
seonjune1015 <kawest@kaist.ac.kr>
2023-06-03 00:32:30 +0900

    230603 : FT

--- app/src/main/java/com/example/pj4test/MainActivity.kt
+++ app/src/main/java/com/example/pj4test/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.pj4test
 
 import android.Manifest.permission.CAMERA
 import android.Manifest.permission.RECORD_AUDIO
+import android.Manifest.permission.BLUETOOTH_CONNECT
 import android.content.Context
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,29 +14,20 @@ import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import java.util.*
-import kotlin.math.pow
-import kotlin.math.sqrt
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
     // permissions
-    private val permissions = arrayOf(RECORD_AUDIO, CAMERA)
+    private val permissions = arrayOf(RECORD_AUDIO, CAMERA, BLUETOOTH_CONNECT)
     private val PERMISSIONS_REQUEST = 0x0000001;
-
-    private val sensorManager by lazy {
-        getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         checkPermissions() // check permissions
-
     }
 
     private fun checkPermissions() {
@@ -50,34 +38,4 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             requestPermissions(permissions, PERMISSIONS_REQUEST)
         }
     }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        event?.let{
-            val x = event.values[0]
-            val y = event.values[1]
-            val z = event.values[2]
-            val r = sqrt(x.pow(2) + y.pow(2) + z.pow(2))
-
-            Log.d("TAG", "onSensorChanged: x: $x, y: $y, z: $z, R: $r")
-        }
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-//        TODO("Not yet implemented")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_FASTEST
-        )
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
-    }
-
 }
